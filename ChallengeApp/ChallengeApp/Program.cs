606dfafe007ace8ee4d8233bbbfdb26243997c83
Bart75r <bbachusz@gmail.com>
2023-01-18 20:17:33 +0100

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Ewa";
+string plec = "kobieta";
+var wiek = 18;
+if (plec == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (plec != "kobieta" && wiek < 18)
+{
+     Console.WriteLine("Niepełnoletni mężczyzna");
+}
+if (imie != "Ewa" && plec != "kobieta" && wiek != 33)
+{
+    Console.WriteLine("Żaden warunek nie jest spełniony");
+}
