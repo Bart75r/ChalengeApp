efe35c90879711aa0fa4c6a9997679cfe42bb9b3
Bart75r <bbachusz@gmail.com>
2023-01-18 20:47:36 +0100

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,27 @@
 ﻿string imie = "Ewa";
 string plec = "kobieta";
-var wiek = 18;
+var wiek = 33;
 if (plec == "kobieta" && wiek < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
 }
 if (imie == "Ewa" && wiek == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
+else
+{
+    Console.WriteLine("To nie jest Ewa w wieku 33 lat");
+}
 if (plec != "kobieta" && wiek < 18)
 {
-     Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine("Niepełnoletni mężczyzna");
 }
-if (imie != "Ewa" && plec != "kobieta" && wiek != 33)
+else
 {
-    Console.WriteLine("Żaden warunek nie jest spełniony");
+    Console.WriteLine("Pełnoletnia kobieta");
 }
